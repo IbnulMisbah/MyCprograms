66695cbd0b41a6fbc14315a8131b687c170d3ab2
IbnulMisbah <abdullahibnulmisbah@gmail.com>
2025-01-07 16:59:21 +0600

    From Neovim

---
+++ 01Bismillah.c
@@ -0,0 +1,17 @@
+// Bismillahir Rahmanir Rahim ..
+// Its my first c program by Neovim ..
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    system("clear");
+
+    printf ("Bismillahir Rahmanir Rahim ..\n");
+
+    printf("Assalamu Alaikum ..\n");
+
+    return  0;
+
+}
---
+++ 02dataype.c
@@ -0,0 +1,16 @@
+// Bismillahir Rahmanir Rahim ..
+// Its my second C program on termix by Neovim ..
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main () {
+    int num1 = 10000 ;
+    float num2 = 626252 ;
+    double num3 = 123333 ;
+    
+    printf ("Bismillahir Rahmanir Rahim .\n");
+    printf ("Int = %d \nFloat = %f \nDouble = %lf ..\n", num1, num2 , num3 );
+
+    return EXIT_SUCCESS;
+}
